d4a57900a3e56e72257b1f6b74af27b715928738
gokulappavu <gokulraj2780@gmail.com>
2024-09-26 11:04:44 +0530

    .env file added

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,13 +1,16 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class DatabaseConnection:
-
     def __init__(self):
         self.conn = mysql.connector.connect(
-            host='127.0.0.1',
-            user='root',
-            password='@Vettriv357',
-            database='atm_database'
+            host=os.getenv('DB_HOST'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            database=os.getenv('DB_NAME')
         )
 
     def get_connection(self):
